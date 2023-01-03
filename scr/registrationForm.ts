8a100b79b0a807d0c6b23c514fafec641f7de2fc
Aleksander Avtushko <aleksandravtusko_gmail_com>
2023-01-04 00:24:20 +0300

    feat: added registration form and unit tests

---
+++ scr/registrationForm.ts
@@ -0,0 +1,60 @@
+export class RegistrationForm {
+    login: any;
+    password: any;
+    name: string;
+    age: number;
+    sex: string;
+
+    constructor(login: any, password: any, name: string, age: number, sex: string) {
+        this.login = login;
+        this.password = password;
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    MaxMinSymbolsLog() {
+        if (this.login.length > 20) {
+            return "We can enter max 20 symbols";
+        } else if (this.login.length < 2) {
+            return "We should add min 2 symbols";
+        } else {
+            return `This login name: ${this.login} is correct`;
+        }
+    }
+
+    MaxMinSymbolsPassword() {
+        if (this.password.length > 20) {
+            return "We can enter max 20 symbols";
+        } else if (this.password.length < 8) {
+            return "We should add min 8 symbols";
+        } else {
+            return `This password: ${this.password} is correct`;
+        }
+    }
+
+    MinAge() {
+        if (this.age < 18) {
+            return "You must be over 18";
+        } else {
+            return `You have more then ${this.age} age. You can check this site`;
+        }
+    }
+
+    SelectorSex() {
+        if (this.sex != ("Male" || "Female")) {
+            return "enter correct sex Male or Female";
+        } else {
+            return this.sex;
+        }
+    }
+    NameValue() {
+        if (this.name.length < 2) {
+            return `The name cannot be empty or have less than 2 characters`;
+        } else if (this.name.length > 25) {
+            return `The name cannot be have more than 25 characters`;
+        } else {
+            return this.name;
+        }
+    }
+}
---
+++ scr/uniteTestsForRegistrationForm.spec.ts
@@ -0,0 +1,91 @@
+import { RegistrationForm } from "./registrationForm";
+
+const testRegFormWithMinVal = new RegistrationForm("S", "Qwer", "Y", 17, "Gmale");
+const testRegFormWithMaxVal = new RegistrationForm(
+    "Stepanenko500000000000",
+    "Qwerty1234567890`1234",
+    "StepanenkaArraratovnaDominatorovna",
+    17,
+    "Gmale",
+);
+const testRegFormWithCorrectVal = new RegistrationForm("Stepanenko@mail.ru", "Qwerty123!", "Stepanenka", 25, "Female");
+
+describe("Negative Tests", function () {
+    test("Check message if login < then 2 symbols", function () {
+        expect(testRegFormWithMinVal.MaxMinSymbolsLog()).toBe("We should add min 2 symbols");
+    });
+
+    test("Check message if login > then 20 symbols", function () {
+        expect(testRegFormWithMaxVal.MaxMinSymbolsLog()).toBe("We can enter max 20 symbols");
+    });
+
+    test("Check valid login message doesn't display if value incorrect", function () {
+        expect(testRegFormWithMinVal.MaxMinSymbolsLog()).not.toBe(
+            `This login name: ${testRegFormWithMinVal.login} is correct`,
+        );
+    });
+
+    test("Check message if password < then 8 symbols", function () {
+        expect(testRegFormWithMinVal.MaxMinSymbolsPassword()).toBe("We should add min 8 symbols");
+    });
+
+    test("Check message if password > then 20 symbols", function () {
+        expect(testRegFormWithMaxVal.MaxMinSymbolsPassword()).toBe("We can enter max 20 symbols");
+    });
+
+    test("Check valid password message doesn't display if value incorrect", function () {
+        expect(testRegFormWithMinVal.MaxMinSymbolsPassword()).not.toBe(
+            `This password ${testRegFormWithMinVal.password} is correct`,
+        );
+    });
+
+    test("Check message if age < then 18 symbols", function () {
+        expect(testRegFormWithMinVal.MinAge()).toBe("You must be over 18");
+    });
+
+    test("Check valid age message doesn't display if value incorrect", function () {
+        expect(testRegFormWithMinVal.MinAge()).not.toBe(
+            `You have more then ${testRegFormWithMinVal.age} age. You can check this site`,
+        );
+    });
+
+    test("Check message if sex not equal Male or Female", function () {
+        expect(testRegFormWithMinVal.SelectorSex()).toBe("enter correct sex Male or Female");
+    });
+
+    test("Check message if name < then 2 characters", function () {
+        expect(testRegFormWithMinVal.NameValue()).toBe("The name cannot be empty or have less than 2 characters");
+    });
+
+    test("Check message if name > then 25 characters", function () {
+        expect(testRegFormWithMaxVal.NameValue()).toBe("The name cannot be have more than 25 characters");
+    });
+});
+
+describe("Positive tests value", function () {
+    test("Check valid login", function () {
+        expect(testRegFormWithCorrectVal.MaxMinSymbolsLog()).toBe(
+            `This login name: ${testRegFormWithCorrectVal.login} is correct`,
+        );
+    });
+
+    test("Check valid password value", function () {
+        expect(testRegFormWithCorrectVal.MaxMinSymbolsPassword()).toBe(
+            `This password: ${testRegFormWithCorrectVal.password} is correct`,
+        );
+    });
+
+    test("Check valid age value", function () {
+        expect(testRegFormWithCorrectVal.MinAge()).toBe(
+            `You have more then ${testRegFormWithCorrectVal.age} age. You can check this site`,
+        );
+    });
+
+    test("Check valid sex value", function () {
+        expect(testRegFormWithCorrectVal.SelectorSex()).toBe(`${testRegFormWithCorrectVal.sex}`);
+    });
+
+    test("Check valid name value", function () {
+        expect(testRegFormWithCorrectVal.NameValue()).toBe(`${testRegFormWithCorrectVal.name}`);
+    });
+});
