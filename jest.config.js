48801416ef8e922cb2878129a056aa53ca2912c0
Aleksander Avtushko <aleksandravtusko_gmail_com>
2023-01-04 23:42:53 +0300

    feat: added API tests and change config

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,5 @@
 module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
-    testMatch: ["**/testJest/*apiTests.spec.ts"],
+    testMatch: ["**/testJest/*.spec.ts"],
 };
---
+++ testJest/apiTests.spec.ts
@@ -0,0 +1,132 @@
+import superagent from "superagent";
+describe("Check get request", () => {
+    test("Check get SINGLE RESOURCE", async () => {
+        const res = await superagent.get("https://reqres.in/api/unknown/2");
+        expect(res.status).toBe(200);
+    });
+    test("Check get LIST RESOURCE", async () => {
+        const res = await superagent.get("https://reqres.in/api/unknown");
+        expect(res.status).toBe(200);
+    });
+
+    test("Check get LIST RESOURCE", async () => {
+        const res = await superagent.get("https://jsonplaceholder.typicode.com/posts/1");
+        expect(res.status).toBe(200);
+    });
+
+    test("SINGLE USER NOT FOUND", async () => {
+        let res: any;
+        try {
+            res = await superagent.get("https://reqres.in/api/users/23");
+        } catch (error: any) {
+            expect(error.status).toBe(404);
+        }
+    });
+
+    test("SINGLE RESOURCE NOT FOUND", async () => {
+        let res: any;
+        try {
+            res = await superagent.get("https://reqres.in/api/unknown/23");
+        } catch (error: any) {
+            expect(error.status).toBe(404);
+        }
+    });
+});
+
+describe("Check POST request", () => {
+    test("Add test data", async () => {
+        let res: any;
+        try {
+            res = await superagent
+                .post("https://jsonplaceholder.typicode.com/posts")
+                .set("content-type", "application/json")
+                .send({
+                    userId: "12",
+                    id: "20",
+                    title: "Test#1",
+                    body: "est rerum tempore vitae nsequi sint nihil reprehenderit dolor",
+                });
+        } catch (error: any) {
+            expect(error.message).toBe("Bad Request");
+        }
+        expect(res.status).toBe(201);
+    });
+
+    test("LOGIN - SUCCESSFUL", async () => {
+        let res: any;
+        try {
+            res = await superagent.post("https://reqres.in/api/login").set("content-type", "application/json").send({
+                email: "peter@klaven",
+            });
+        } catch (error: any) {
+            expect(error.status).toBe(400);
+        }
+    });
+
+    test("LOGIN - SUCCESSFUL", async () => {
+        let res: any;
+        try {
+            res = await superagent.post("https://reqres.in/api/login").set("content-type", "application/json").send({
+                email: "eve.holt@reqres.in",
+                password: "cityslicka",
+            });
+        } catch (error: any) {
+            expect(error.message).toBe("Bad Request");
+        }
+        expect(res.status).toBe(200);
+    });
+});
+
+describe("Check DELETE request", () => {
+    test("Delete value from jsonplaceholder", async () => {
+        let res: any;
+        try {
+            res = await superagent.delete("https://jsonplaceholder.typicode.com/posts/1");
+        } catch (error: any) {
+            throw new Error(error);
+        }
+        expect(res.status).toBe(200);
+        expect(res.body).toBeDefined();
+    });
+
+    test("Delete value from reqres.in", async () => {
+        let res: any;
+        try {
+            res = await superagent.delete("https://reqres.in/api/users/2");
+        } catch (error: any) {
+            throw new Error(error);
+        }
+        expect(res.status).toBe(204);
+        expect(res.body).toBeDefined();
+    });
+});
+
+describe("Check PUT request", () => {
+    test("put value to reqres.in", async () => {
+        let res: any;
+        try {
+            res = await superagent.put("https://reqres.in/api/users/2").set("content-type", "application/json").send({
+                name: "Valeron",
+                job: "Dvornik",
+            });
+        } catch (error: any) {
+            expect(error.message).toBe("Bad Request");
+        }
+        expect(res.status).toBe(200);
+    });
+
+    test("put value to jsonplaceholder", async () => {
+        let res: any;
+        try {
+            res = await superagent.put("https://reqres.in/api/users/2").set("content-type", "application/json").send({
+                userId: 55,
+                id: 15,
+                title: "add new value",
+                body: "Test body",
+            });
+        } catch (error: any) {
+            expect(error.message).toBe("Bad Request");
+        }
+        expect(res.status).toBe(200);
+    });
+});
